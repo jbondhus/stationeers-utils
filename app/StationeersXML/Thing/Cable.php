1ebf907930e608a57b433e83de4ea3078c211719
Jonathan Bondhus <jonathan.bondhus@gmail.com>
2019-05-18 18:33:38 -0500

    Add cable network ID as custom attribute

--- app/StationeersXML/Thing/Cable.php
+++ app/StationeersXML/Thing/Cable.php
@@ -9,8 +9,12 @@ class Cable extends Thing
 {
     public const TYPE = 'cable';
 
+    protected $cable_network_id;
+
     public function __construct($dom_element)
     {
         parent::__construct($dom_element);
+
+        $this->cable_network_id = $this->get_node_value('CableNetworkId');
     }
 }
