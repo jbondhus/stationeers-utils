a8e2f60dbd17c6130928940ebee2779c38538dd3
Jonathan Bondhus <jonathan.bondhus@gmail.com>
2019-05-25 14:19:18 -0500

    Add autocomplete attributes

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,11 +4,11 @@
     <form id="login-form" onsubmit="event.preventDefault();">
         <div class="form-group">
             <label for="email-input">Email address</label>
-            <input type="email" class="form-control" id="email-input" placeholder="Enter email">
+            <input type="email" class="form-control" id="email-input" autocomplete="username email" placeholder="Enter email">
         </div>
         <div class="form-group">
             <label for="password-input">Password</label>
-            <input type="password" class="form-control" id="password-input" placeholder="Password">
+            <input type="password" class="form-control" id="password-input" autocomplete="current-password" placeholder="Password">
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
     </form>
